215ccad9c665402f47fa8f6132b066f6f038a676
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2021-12-15 19:34:14 -0500

    Uso de Componentes en un mismo archivo.

--- code/react-basic/src/App.js
+++ code/react-basic/src/App.js
@@ -1,15 +1,26 @@
-import Dish, * as D from './components/dish';
-import Header from './components/header';
+import React from 'react';
 import './styles/App.css';
-import { Flag } from './components/dish';
+
+const todos = [
+  { text:'Cortar cebolla', completed:false },
+  { text:'Tomar un curso de Javascript Avanzado', completed:false },
+  { text:'Llorar con la llorona', completed:false },
+]
 
 function App() {
   return (
-    <div className="App">
-      <Header/> 
-      <Dish></Dish>
-      <Flag></Flag>
-    </div>
+    <React.Fragment>
+      {/*<TodoCounter/>*/}
+      <h2>Has completado dos de tres tareas.</h2>
+      {/*<TodoSearch/>*/}
+      <input placeholder="Cebolla"></input>
+      {/*<TodoList>
+        todos.map( do => (
+          <TodoItem/>
+        ) )
+      </TodoList>*/}
+      <button>*</button>
+    </React.Fragment>
   );
 }
 
