bcaeffa524fb8b70979adcc43b5fb3588124638e
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2022-01-14 20:33:36 -0500

    Asincronismo - intro

---
+++ code/javascript-asincronismo/taller_01.js
@@ -0,0 +1,11 @@
+console.log('Primera frase.')
+
+setTimeout(function(){
+    console.log('Segunda frase.')
+}, 0)
+
+setTimeout(function(){
+    console.log('Tercera frase.')
+}, 0)
+
+console.log('Cuarta frase.')
