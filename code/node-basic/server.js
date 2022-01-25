5f015575b1287402a86259c970fcba99f57b35e0
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2022-01-25 18:49:23 -0500

    Personalizacion de respuestas.

--- code/node-basic/server.js
+++ code/node-basic/server.js
@@ -1,14 +1,33 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const router = express.Router()
 
 let app = express()
+
+app.use( bodyParser.json() )
+app.use( bodyParser.urlencoded({extended:false}) )
 app.use( router )
 
-router.get('/', function(req, res) {
-    res.send('Hola desde GET.')
+router.get('/mensaje', function(req, res) {
+    // esto es lo que recibo del cliente.
+    console.log(req.headers)
+
+    // esto es lo que yo envio desde el servidor
+    res.header({'custom-header':'Nuestro valor personalizado.'})
+    res.send('Lista de mensajes.')
 })
-router.post('/', function(req, res){
+
+router.post('/mensaje', function(req, res){
     res.send('Hola desde POST.')
+})
+
+router.delete('/mensaje', function(req, res) {
+    // Esto es lo que recibo del cliente.
+    console.log( req.query )
+    console.log( req.body )
+    
+    // Esto es lo que envio al cliente.
+    res.send('Mensaje eliminado correctamente. ' + req.body.text)
 })
 
 let puerto = 3000
