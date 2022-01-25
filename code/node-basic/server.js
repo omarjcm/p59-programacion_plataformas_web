69865702bd01eb4e78ada90a5aace86de7b20321
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2022-01-25 18:31:15 -0500

    Peticiones GET y POST.

--- code/node-basic/server.js
+++ code/node-basic/server.js
@@ -1,10 +1,16 @@
 const express = require('express')
+const router = express.Router()
 
 let app = express()
-app.use('/', function(req, res) {
-    res.send('Hola mundo')
-}
-)
+app.use( router )
+
+router.get('/', function(req, res) {
+    res.send('Hola desde GET.')
+})
+router.post('/', function(req, res){
+    res.send('Hola desde POST.')
+})
+
 let puerto = 3000
 app.listen(puerto)
 console.log(`La aplicacion esta escuchando en http://localhost:${puerto}`)
